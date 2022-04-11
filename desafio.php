cebe64d91e4dd38ccf91746623e6704acd32c52a
Eliane Santiago <1010eliane1010@gmail.com>
2022-04-11 15:23:25 -0300

    Desafio

---
+++ desafio.php
@@ -0,0 +1,16 @@
+<?php
+
+	// Desafio -> Exibir todos os números impares de 1 à 100
+
+for($num = 0; $num <=100; $num++){
+
+	if($num % 2 != 0){
+		echo $num . PHP_EOL;
+
+		echo "<br/>";
+	}
+}
+
+
+
+?>
