88db28132efe47996ce41a3ee5bf0c5f036dd4ed
Eliane Santiago <1010eliane1010@gmail.com>
2022-04-14 00:00:25 -0300

    criação da nova pasta avançando no php

---
+++ avancando/array_loop.php
@@ -0,0 +1,15 @@
+<?php
+
+// Criando a variável Array
+$idadeList = [21,23,19,21,30,35,25,45];
+
+//Acessando elemento do Array com loop
+
+for($i; $i<count($idadeList); $i++){
+
+	echo $idadeList[$i] . PHP_EOL;
+}
+
+// echo count($idadeList); -> exibe o tamanho do array
+
+?>s
---
+++ avancando/arrays.php
@@ -0,0 +1,10 @@
+<?php
+// Criando a variável Array
+$idadeList = [21,23,19,21,30,35,25,45];
+
+//Acessando elemento do Array
+$primeiraIdade = $idadeList[0];
+
+echo($primeiraIdade);
+
+?>
